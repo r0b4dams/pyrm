4e1a9b2126ff6e5ea982e3c3204035a34bea44d7
Robert Adams <r0b4dams@gmail.com>
2024-02-12 23:07:46 -0800

    Update to allow zero, one, or many args

--- src/commands/install.py
+++ src/commands/install.py
@@ -1,25 +1,27 @@
-"""install"""
+"""
+commands.install
+"""
 
 import os
-from utils import create_requirements, save_requirements
+from utils import create_reqs, save_reqs
 from config import PIP, REQS_PATH, VENV_PATH
-from .clean import clean
 
 
 def install(args):
     """
     Calls pip to install the given package
 
-    Installs all packages if no package given
+    Installs packages listed in requirements if no package given
     """
-    clean()
     os.system(f"python3 -m venv {VENV_PATH}")
 
     if not os.path.exists(REQS_PATH):
-        create_requirements()
+        create_reqs()
 
-    if args.package is not None:
-        os.system(f"{PIP} install {args.package}")
-        save_requirements()
+    if len(args.package) > 0:
+        packages = " ".join([pkg for pkg in args.package])
+        os.system(f"{PIP} install {packages}")
     else:
         os.system(f"{PIP} install -r {REQS_PATH}")
+
+    save_reqs()
