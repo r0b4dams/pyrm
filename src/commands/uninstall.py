5f60d81fcbe0c117ed6128c6186fa94b8b3aa7e6
Robert Adams <r0b4dams@gmail.com>
2024-02-14 01:39:42 -0800

    Add uninstall command

--- src/commands/uninstall.py
+++ src/commands/uninstall.py
@@ -3,14 +3,32 @@ commands.uninstall
 """
 
 import os
-from utils import save_reqs
-from config import PIP
+import json
+from argparse import Namespace
+from config import PIP, VENV_PATH, PROJECT_JSON
 
 
-def uninstall(args):
+def uninstall(args: Namespace) -> None:
     """
     uninstall the given package
     """
-    packages = " ".join(list(args.package))
-    os.system(f"{PIP} uninstall -y {packages}")
-    save_reqs()
+    try:
+        if os.path.exists(VENV_PATH):
+            packages = " ".join(list(args.packages))
+            os.system(f"{PIP} uninstall -y {packages}")
+
+        with open(PROJECT_JSON, "r", encoding="utf-8") as f:
+            doc = json.load(f)
+
+            if doc and doc["requirements"]:
+                doc["requirements"] = {
+                    pkg: ver
+                    for pkg, ver in doc["requirements"].items()
+                    if pkg not in args.packages
+                }
+
+        with open(PROJECT_JSON, "w+", encoding="utf-8") as f:
+            json.dump(doc, f, indent=2)
+
+    except (json.decoder.JSONDecodeError, FileNotFoundError) as e:
+        print(e)
