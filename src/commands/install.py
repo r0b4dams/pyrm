cb848d49d5fa4ea76888739b740fac087de4cb69
Robert Adams <r0b4dams@gmail.com>
2024-02-14 01:13:44 -0800

    Update install method with package.json

--- src/commands/install.py
+++ src/commands/install.py
@@ -3,11 +3,14 @@ commands.install
 """
 
 import os
-from utils import create_reqs, save_reqs
-from config import PIP, REQS_PATH, VENV_PATH
+import subprocess
+import tempfile
+import json
+from argparse import Namespace
+from config import PIP, PROJECT_JSON, VENV_PATH
 
 
-def install(args):
+def install(args: Namespace) -> None:
     """
     Calls pip to install the given package
 
@@ -15,13 +18,61 @@ def install(args):
     """
     os.system(f"python3 -m venv {VENV_PATH}")
 
-    if not os.path.exists(REQS_PATH):
-        create_reqs()
-
-    if len(args.package) > 0:
-        packages = " ".join([pkg for pkg in args.package])
-        os.system(f"{PIP} install {packages}")
+    if len(args.packages) > 0:
+        install_from_args(args.packages)
     else:
-        os.system(f"{PIP} install -r {REQS_PATH}")
+        install_from_json()
 
-    save_reqs()
+
+def install_from_args(packages: list[str]) -> None:
+    """
+    Install given packages via pip
+
+    Creates a bob.yaml file if not found
+    """
+    packages = " ".join(list(packages))
+    os.system(f"{PIP} install {packages}")
+
+    req_bytes = subprocess.check_output([PIP, "freeze"])
+    req_list = req_bytes.decode().splitlines()
+    requirements = dict([pkg.split("==") for pkg in req_list])
+
+    try:
+        with open(PROJECT_JSON, "r", encoding="utf-8") as f:
+            doc = json.load(f)
+            doc["requirements"] = requirements
+
+        with open(PROJECT_JSON, "w", encoding="utf-8") as f:
+            json.dump(doc, f, sort_keys=False, indent=2)
+
+    except FileNotFoundError:
+        with open(PROJECT_JSON, "w+", encoding="utf-8") as f:
+            json.dump({"requirements": requirements}, f, indent=2)
+
+
+def install_from_json() -> None:
+    """
+    Get dependency list from bob.json and install
+    """
+    fd, tmp_req_path = tempfile.mkstemp()
+    reqs = ""
+
+    try:
+        with open(PROJECT_JSON, "r", encoding="utf-8") as f:
+            doc = json.load(f)
+            deps = doc["requirements"]
+            reqs = "\n".join([f"{pkg}=={ver}" for pkg, ver in deps.items()])
+
+    except FileNotFoundError:
+        print(f"unable to install: {PROJECT_JSON} not found")
+
+    except (KeyError, AttributeError):
+        print("unable to install: requirements not found")
+
+    else:
+        with os.fdopen(fd, "w") as tmp:
+            tmp.write(reqs)
+        os.system(f"{PIP} install -r {tmp_req_path}")
+
+    finally:
+        os.remove(tmp_req_path)
