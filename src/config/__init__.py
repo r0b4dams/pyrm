5a8a9152eb36840368870fc5774348b14c5afe72
Robert Adams <r0b4dams@gmail.com>
2024-02-14 01:16:25 -0800

    Update metadata filename, add default scripts

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -2,8 +2,10 @@
 config
 """
 
-REQS_PATH = ".requirements.txt"
 VENV_PATH = ".venv"
+PROJECT_JSON = "project.json"
 
 PY = f"{VENV_PATH}/bin/python3"
 PIP = f"{VENV_PATH}/bin/pip"
+
+DEFAULT_SCRIPTS = {"hello": "echo Hello, World!"}
