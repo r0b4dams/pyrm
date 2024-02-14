c62df04df45a98e8ae12f21100aef142e898f4cb
Robert Adams <r0b4dams@gmail.com>
2024-02-13 19:49:37 -0800

    Pluralize arg

--- src/__main__.py
+++ src/__main__.py
@@ -3,25 +3,25 @@
 """
 A simple CLI to manage dependencies in a Python project
 """
+
 import argparse
 import commands
 
 parser = argparse.ArgumentParser(prog="bob", description="Python dev environment CLI")
-
+subparsers = parser.add_subparsers()
 
 # commands
-subparsers = parser.add_subparsers()
 
 cmd_run = subparsers.add_parser("run")
 cmd_run.add_argument("script", nargs=1)
 cmd_run.set_defaults(func=commands.run)
 
 cmd_add = subparsers.add_parser("install")
-cmd_add.add_argument("package", nargs="*", default=None)
+cmd_add.add_argument("packages", nargs="*", default=None)
 cmd_add.set_defaults(func=commands.install)
 
 cmd_remove = subparsers.add_parser("uninstall")
-cmd_remove.add_argument("package", nargs="+")
+cmd_remove.add_argument("packages", nargs="+")
 cmd_remove.set_defaults(func=commands.uninstall)
 
 cmd_clean = subparsers.add_parser("clean")
