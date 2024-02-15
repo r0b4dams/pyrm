15662c68fe1afc575cb28358bc6e44f95ccdae47
Robert Adams <r0b4dams@gmail.com>
2024-02-14 16:31:47 -0800

    Refactor err handling

--- cli/commands/uninstall.py
+++ cli/commands/uninstall.py
@@ -6,29 +6,33 @@ import os
 import json
 from argparse import Namespace
 from config import PIP, VENV_PATH, PROJECT_JSON
+from utils import get_requirements
 
 
 def uninstall(args: Namespace) -> None:
     """
-    uninstall the given package
+    Calls pip to uninstall the given package
     """
-    try:
-        if os.path.exists(VENV_PATH):
-            packages = " ".join(list(args.packages))
-            os.system(f"{PIP} uninstall -y {packages}")
+    if not os.path.exists(VENV_PATH):
+        print(f"unable to uninstall: {VENV_PATH} not found")
+        return
 
+    try:
         with open(PROJECT_JSON, "r", encoding="utf-8") as f:
             doc = json.load(f)
 
-            if doc and doc["requirements"]:
-                doc["requirements"] = {
-                    pkg: ver
-                    for pkg, ver in doc["requirements"].items()
-                    if pkg not in args.packages
-                }
+        if not isinstance(doc, dict):
+            raise TypeError(f"{PROJECT_JSON} must be dict")
+
+        packages = " ".join(list(args.packages))
+        os.system(f"{PIP} uninstall -y {packages}")
+        doc["requirements"] = get_requirements()
 
         with open(PROJECT_JSON, "w+", encoding="utf-8") as f:
             json.dump(doc, f, indent=2)
 
     except (json.decoder.JSONDecodeError, FileNotFoundError) as e:
-        print(e)
+        print(f"unable to parse {PROJECT_JSON}: {e}")
+
+    except TypeError as type_err:
+        print(f"unable to uninstall -> {type_err}")
