8292a1acee17506940151b20108c1302f6a52fed
Robert Adams <r0b4dams@gmail.com>
2024-02-14 17:42:14 -0800

    Move helper fns to utils

--- cli/commands/init.py
+++ cli/commands/init.py
@@ -2,11 +2,11 @@
 commands.init
 """
 
-import os
 import json
 from argparse import Namespace
 from typing import Tuple
-from config import PROJECT_JSON, DEFAULT_SCRIPTS
+from utils import generate_root, generate_entrypoint
+from config import PROJECT_JSON
 
 
 def init(args: Namespace) -> None:
@@ -23,7 +23,7 @@ def init(args: Namespace) -> None:
         print(json.dumps(project_data, indent=2))
 
         if not args.y and input("Is this OK? (yes) "):
-            print("Aborted")
+            print("Abort mission")  # quit if anything other than empty str
             return
 
     except (KeyboardInterrupt, EOFError):
@@ -32,52 +32,15 @@ def init(args: Namespace) -> None:
     else:
         with open(PROJECT_JSON, "w+", encoding="utf-8") as f:
             json.dump(project_data, f, indent=2)
-
         generate_entrypoint(project_data)
-
-
-def generate_entrypoint(project_data: dict):
-    """
-    Create a source folder with app entrypoint
-    """
-    src_path = os.path.join(os.getcwd(), "src")
-
-    if not os.path.exists(src_path):
-        os.makedirs(src_path)
-
-    mod_doc_str = project_data["name"] if project_data["name"] else "A new project!"
-    fn_doc_str = "Run the app with `pyrob run`"
-    with open(f"{src_path}/main.py", "w+", encoding="utf-8") as main:
-        main.write(f'"""\n{mod_doc_str}\n"""\n\n')
-        main.write("def main():\n")
-        main.write(f'    """\n    {fn_doc_str}\n    """\n')
-        main.write('    print("Hello, World!")\n\n')
-        main.write("main()\n")
-
-
-def generate_root():
-    """
-    Setup a dict to create a project.json file
-    """
-    *_, current_folder_name = os.path.split(os.getcwd())
-
-    root = {
-        "name": current_folder_name,
-        "version": "0.0.1",
-        "author": "bob",  # get from git?
-        "description": "a new project",
-        "scripts": DEFAULT_SCRIPTS,
-    }
-
-    return root
 
 
 def defaults() -> Tuple[dict, dict]:
     """
     Create a dict with some default data
     """
-    data = None
     root = generate_root()
+    data = {**root}
 
     try:
         with open(PROJECT_JSON, "r", encoding="utf-8") as f:
@@ -85,8 +48,6 @@ def defaults() -> Tuple[dict, dict]:
 
         if not isinstance(doc, dict):
             raise TypeError(f"{PROJECT_JSON} must be dict")
-
-        data = {**root}  # preserve order of root keys
 
         # for the root keys,
         # if value falsy go with root value
