b44074fce226cb8488b949e95794aeeb3f13d6e4
Robert Adams <r0b4dams@gmail.com>
2024-02-15 10:14:57 -0800

    Add setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="pyrob",
+    version="0.0.1",
+    author="r0b4dams",
+    description="This runs my script which is great.",
+    entry_points={"console_scripts": ["pyrob=cli:main"]},
+    python_requires=">=3.10",
+    setup_requires=["setuptools"],
+    package_dir={"": "cli"},
+    packages=setuptools.find_packages("cli"),
+)
