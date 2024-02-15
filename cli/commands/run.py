d2fb12e57e5e254e138355ddbeee38fb52518078
Robert Adams <r0b4dams@gmail.com>
2024-02-14 17:45:14 -0800

    Add run script

--- cli/commands/run.py
+++ cli/commands/run.py
@@ -6,23 +6,27 @@ import os
 import json
 from config import PROJECT_JSON
 
-# TODO if no args given run main.py
-
 
 def run(args) -> None:
     """
     Look for a script in project.json and run it if found
+
+    Runs src/main/py if no script given
     """
-    (script,) = args.script
+    script = args.script
 
-    try:
-        with open(PROJECT_JSON, "r", encoding="utf-8") as f:
-            doc = json.load(f)
+    if not script:
+        os.system("python3 src/main.py")
 
-            if script in doc["scripts"] and doc["scripts"] is not None:
-                os.system(doc["scripts"][script])
-            else:
-                print(f'script "{script}" not found in {PROJECT_JSON} requirements')
+    else:
+        try:
+            with open(PROJECT_JSON, "r", encoding="utf-8") as f:
+                doc = json.load(f)
 
-    except (json.decoder.JSONDecodeError, FileNotFoundError) as e:
-        print(e)
+                if doc["scripts"] and script in doc["scripts"]:
+                    os.system(doc["scripts"][script])
+                else:
+                    print(f'script "{script}" not found in {PROJECT_JSON} requirements')
+
+        except (json.decoder.JSONDecodeError, FileNotFoundError) as e:
+            print(f"unable to run command -> {e}")
