64b65846f0fcd37d0e7d9cf663b5e9560166960e
Robert Adams <r0b4dams@gmail.com>
2024-02-14 16:31:25 -0800

    Update init to create entrypoint

--- cli/commands/init.py
+++ cli/commands/init.py
@@ -3,7 +3,6 @@ commands.init
 """
 
 import os
-import sys
 import json
 from argparse import Namespace
 from typing import Tuple
@@ -14,43 +13,97 @@ def init(args: Namespace) -> None:
     """
     Initialize a project and add a project.json file containing project data
     """
-    project_data = defaults() if args.y else prompts()
+    try:
+        if args.y:
+            project_data, *_ = defaults()
+        else:
+            project_data = prompts()
 
-    if "scripts" not in project_data:
-        project_data["scripts"] = DEFAULT_SCRIPTS
+        print("Initializing project with:")
+        print(json.dumps(project_data, indent=2))
 
-    with open(PROJECT_JSON, "w+", encoding="utf-8") as f:
-        json.dump(project_data, f, indent=2)
+        if not args.y and input("Is this OK? (yes) "):
+            print("Aborted")
+            return
 
-    # TODO create a src folder if not exists
+    except (KeyboardInterrupt, EOFError):
+        print("\nexit init")
+
+    else:
+        with open(PROJECT_JSON, "w+", encoding="utf-8") as f:
+            json.dump(project_data, f, indent=2)
+
+        generate_entrypoint(project_data)
+
+
+def generate_entrypoint(project_data: dict):
+    """
+    Create a source folder with app entrypoint
+    """
+    src_path = os.path.join(os.getcwd(), "src")
+
+    if not os.path.exists(src_path):
+        os.makedirs(src_path)
+
+    mod_doc_str = project_data["name"] if project_data["name"] else "A new project!"
+    fn_doc_str = "Run the app with `pyrob run`"
+    with open(f"{src_path}/main.py", "w+", encoding="utf-8") as main:
+        main.write(f'"""\n{mod_doc_str}\n"""\n\n')
+        main.write("def main():\n")
+        main.write(f'    """\n    {fn_doc_str}\n    """\n')
+        main.write('    print("Hello, World!")\n\n')
+        main.write("main()\n")
+
+
+def generate_root():
+    """
+    Setup a dict to create a project.json file
+    """
+    *_, current_folder_name = os.path.split(os.getcwd())
+
+    root = {
+        "name": current_folder_name,
+        "version": "0.0.1",
+        "author": "bob",  # get from git?
+        "description": "a new project",
+        "scripts": DEFAULT_SCRIPTS,
+    }
+
+    return root
 
 
 def defaults() -> Tuple[dict, dict]:
     """
     Create a dict with some default data
     """
-    *_, current_folder_name = os.path.split(os.getcwd())
-
     data = None
-    root = {
-        "name": current_folder_name,
-        "version": "0.0.1",
-        "author": "bob",  # get from git?
-        "description": "a new project",
-    }
+    root = generate_root()
 
     try:
-        with open(PROJECT_JSON, "r+", encoding="utf-8") as f:
-            doc: dict[str, str] = json.load(f)
-        if doc:
-            data = {**root}
-            for k, v in doc.items():
-                str_not_empty = isinstance(v, str) and not v.strip()
-                if k in root and str_not_empty:
-                    data[k] = root[k]
+        with open(PROJECT_JSON, "r", encoding="utf-8") as f:
+            doc = json.load(f)
 
-    except (json.decoder.JSONDecodeError, FileNotFoundError):
+        if not isinstance(doc, dict):
+            raise TypeError(f"{PROJECT_JSON} must be dict")
+
+        data = {**root}  # preserve order of root keys
+
+        # for the root keys,
+        # if value falsy go with root value
+        # else put back k/v found in doc as-is
+        for k, v in doc.items():
+            v_is_empty_str = isinstance(v, str) and not v.strip()
+            if k in root and v_is_empty_str:
+                data[k] = root[k]
+            else:
+                data[k] = v
+
+    except FileNotFoundError:
         pass
+
+    except (json.decoder.JSONDecodeError, TypeError) as e:
+        print(f"{PROJECT_JSON} found but unable to parse -> {e}")
+        print("using defaults")
 
     return data, root
 
@@ -64,15 +117,11 @@ def prompts() -> dict:
     if not data:
         data = root
 
-    try:
-        for key in root:
-            res = input(f"{key} ({data[key]}): ")
-            if res:
-                data[key] = res
+    for key in root:
+        if key == "scripts":
+            continue
+        res = input(f"{key} ({data[key]}): ")
+        if res:
+            data[key] = res
 
-    except (KeyboardInterrupt, EOFError):
-        print()
-        sys.exit()
-
-    else:
-        return data
+    return data
