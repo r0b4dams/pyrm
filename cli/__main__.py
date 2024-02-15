50aaed6ee2c5867f97e5e18ab5bba39b11e7b4f8
Robert Adams <r0b4dams@gmail.com>
2024-02-14 17:41:19 -0800

    Rename entrypoint to __main__

---
+++ cli/__main__.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+"""
+A simple CLI to manage dependencies in a Python project
+"""
+
+import argparse
+import commands
+
+
+def main():
+    """
+    entrypoint
+    """
+    parser = argparse.ArgumentParser(
+        prog="bob", description="Python dev environment CLI"
+    )
+    subparsers = parser.add_subparsers()
+
+    cmd_init = subparsers.add_parser("init")
+    cmd_init.add_argument("-y", default=False, action="store_true")
+    cmd_init.set_defaults(func=commands.init)
+
+    cmd_run = subparsers.add_parser("run")
+    cmd_run.add_argument("script", action="store", default=None, nargs="?")
+    cmd_run.set_defaults(func=commands.run)
+
+    cmd_add = subparsers.add_parser("install")
+    cmd_add.add_argument("packages", nargs="*", default=None)
+    cmd_add.set_defaults(func=commands.install)
+
+    cmd_remove = subparsers.add_parser("uninstall")
+    cmd_remove.add_argument("packages", nargs="+")
+    cmd_remove.set_defaults(func=commands.uninstall)
+
+    cmd_clean = subparsers.add_parser("clean")
+    cmd_clean.set_defaults(func=commands.clean)
+
+    args = parser.parse_args()
+
+    if hasattr(args, "func"):
+        args.func(args)
+
+
+main()
