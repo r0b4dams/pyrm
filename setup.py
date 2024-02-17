28b1a140e645e4420237aa964ee797a0dc755dd3
Robert Adams <r0b4dams@gmail.com>
2024-02-17 00:32:30 -0800

    Update setup script

--- setup.py
+++ setup.py
@@ -1,13 +1,3 @@
 import setuptools
 
-setuptools.setup(
-    name="pyrob",
-    version="0.0.1",
-    author="r0b4dams",
-    description="pyrob package manager",
-    entry_points={"console_scripts": ["pyrob=cli:main"]},
-    python_requires=">=3.10",
-    setup_requires=["setuptools"],
-    package_dir={"": "cli"},
-    packages=setuptools.find_packages("cli"),
-)
+setuptools.setup()
