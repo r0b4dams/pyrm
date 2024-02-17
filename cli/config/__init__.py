0fff898c20b49a38729becc75882827527673871
Robert Adams <r0b4dams@gmail.com>
2024-02-16 20:31:43 -0800

    Rename var

--- cli/config/__init__.py
+++ cli/config/__init__.py
@@ -5,7 +5,8 @@ config
 VENV_PATH = ".venv"
 PROJECT_JSON = "project.json"
 
-PY = f"{VENV_PATH}/bin/python3"
+PYTHON = f"{VENV_PATH}/bin/python3"
 PIP = f"{VENV_PATH}/bin/pip"
+
 
 DEFAULT_SCRIPTS = {"foo": "echo foo bar baz"}
