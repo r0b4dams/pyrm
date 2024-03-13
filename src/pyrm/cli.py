e8d068efbcfe6f82c8725ecbad405d519176b50e
Robert Adams <r0b4dams@gmail.com>
2024-03-13 16:30:55 -0700

    Add version command

--- src/pyrm/cli.py
+++ src/pyrm/cli.py
@@ -2,9 +2,21 @@
 TODO: doc string
 """
 
+from argparse import ArgumentParser
+from . import __version__
+
 
 def main():
     """
     TODO: doc string
     """
-    print("main")
+    parser = ArgumentParser(
+        prog="pyrm", description="A CLI to manage dependencies in a Python project"
+    )
+    parser.add_argument(
+        "-v", "--version", action="version", version=f"%(prog)s {__version__}"
+    )
+
+    subparsers = parser.add_subparsers()
+
+    args = parser.parse_args()
