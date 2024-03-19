007204a06e38ca83e04f96d13ae73498032189dc
Robert Adams <r0b4dams@gmail.com>
2024-03-18 20:12:58 -0700

    Update test to use TemporaryDirectory

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,12 +1,9 @@
 import os
-import shutil
+import tempfile
 from src.pyrob.utils import create_venv
 
-
-def test_create_env():
-    _, current_folder_name = os.path.split(os.getcwd())
-    test_path = f"{current_folder_name}/test_venv"
-    create_venv(test_path)
-    assert os.path.exists(test_path)
-    assert os.path.exists(f"{test_path}/bin/python3")
-    shutil.rmtree(test_path)
+def test_create_venv():
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        create_venv(tmpdirname)
+        assert os.path.exists(tmpdirname)
+        assert os.path.exists(f"{tmpdirname}/bin/python3")
