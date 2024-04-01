d56e365144deafc3cdb7f7be0a97f9a1430f1a60
Robert Adams <r0b4dams@gmail.com>
2024-03-31 18:25:33 -0700

    Add init and git_gitignore fns

--- src/pyrob/utils/git.py
+++ src/pyrob/utils/git.py
@@ -2,7 +2,17 @@
 pyrob.utils.git
 """
 
+import os
+from pyrob.config.vars import GITIGNORE_URL
 from . import run
+
+
+def init() -> None:
+    """
+    Initialize git repository if not exists
+    """
+    if not os.path.exists(".git"):
+        run(["git", "init"])
 
 
 def get_config() -> tuple[str, str]:
@@ -15,3 +25,7 @@ def get_config() -> tuple[str, str]:
     user = run(["git", "config", "user.name"])
     email = run(["git", "config", "user.email"])
     return user, email
+
+
+def get_gitignore() -> None:
+    os.system(" ".join(["curl", "--silent", GITIGNORE_URL, ">", ".gitignore"]))
