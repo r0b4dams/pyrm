b28be79c121e3c18001ca5cbfbb4123c9b2fc028
Robert Adams <r0b4dams@gmail.com>
2024-03-31 18:24:59 -0700

    Add gitignore url

--- src/pyrob/config/vars.py
+++ src/pyrob/config/vars.py
@@ -5,3 +5,6 @@ pyrob.config.vars
 VENV = ".venv"
 PYTHON = f"{VENV}/bin/python3"
 PROJECT_JSON = "project.json"
+GITIGNORE_URL = (
+    "https://raw.githubusercontent.com/github/gitignore/main/Python.gitignore"
+)
