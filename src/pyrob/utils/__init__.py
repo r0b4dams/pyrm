6ff85397240124a1f1888fa7c87b325eae2faa12
Robert Adams <r0b4dams@gmail.com>
2024-03-31 19:49:27 -0700

    Move fns to other modules

--- src/pyrob/utils/__init__.py
+++ src/pyrob/utils/__init__.py
@@ -1,26 +1,3 @@
 """
 pyrob.utils
 """
-
-import subprocess
-
-
-def run(cmd: list[str]) -> str:
-    """
-    Run the given command
-
-    Returns:
-        stdout from subprocess
-    """
-    return subprocess.check_output(cmd).decode().strip()
-
-
-def create_venv(venv: str) -> None:
-    """
-    Create a virtual environment using the venv module
-
-    Args:
-        venv: path to virtual environment directory
-    """
-    print("Creating virtual environment...")
-    run(["python3", "-m", "venv", venv])
